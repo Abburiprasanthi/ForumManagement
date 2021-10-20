28154f229f49529ae50c75d5354aa6738a93e929
Abburi <prasanthi.abburi@capgemini.com>
2021-10-20 11:54:41 +0530

    second

---
+++ ForumManagement/Controllers/UsersController.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ForumManagement.Model;
+using System.Net.Http;
+using System.Net;
+
+namespace ForumManagement.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly ForumContext _context;
+
+        public UsersController(ForumContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Users
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<User>>> GetUsers()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        // GET: api/Users/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<User>> GetUser(long id)
+        {
+            var user = await _context.Users.FindAsync(id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return user;
+        }
+
+        [Route("login")]
+        [HttpPost]
+        public async Task<ActionResult<User>> Login(LoginDetails loginDetails)
+        {
+            //var user = await _context.Users.FindAsync(id);
+
+            var user = _context.Users.Where(x => x.Email.Equals(loginDetails.Email) && x.Password.Equals(loginDetails.Password)).FirstOrDefault();
+
+            if (user == null)
+            {
+                return Ok(new { status = 401, isSuccess = false, message = "Invalid User", });
+                //var msg = new HttpResponseMessage(HttpStatusCode.Unauthorized) { ReasonPhrase = "Invalid credentials!" };
+                //throw new System.Web.Http.HttpResponseException(msg);
+            }
+            else
+
+                return Ok(new { status = 200, isSuccess = true, message = "User Login successfully", User = user });
+            //return user;
+        }
+
+
+        // PUT: api/Users/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutUser(long id, User user)
+        {
+            if (id != user.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(user).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UserExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Users
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<User>> PostUser(User user)
+        {
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetUser", new { id = user.Id }, user);
+        }
+
+        // DELETE: api/Users/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteUser(long id)
+        {
+            var user = await _context.Users.FindAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            _context.Users.Remove(user);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool UserExists(long id)
+        {
+            return _context.Users.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ ForumManagement/Model/Question.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace ForumManagement.Model
+{
+    public class Question
+    {
+        
+        public long id { get; set; }
+
+        public string title { get; set; }
+
+        public string description { get; set; }
+
+        //public string author { get; set; }
+
+        [ForeignKey("User")]
+        [DatabaseGeneratedAttribute(DatabaseGeneratedOption.None)]
+        public long? UserId { get; set; }
+        public User User { get; set; }
+
+        //public ICollection<Answer> Answers { get; set; }
+        public virtual List<Answer> Answers { get; set; }
+
+        public virtual List<VoteOnQuestion> Votes { get; set; }
+    }
+}
